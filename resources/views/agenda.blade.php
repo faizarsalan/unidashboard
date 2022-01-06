f4fa24479177ccfc620a8d02c9219042814ae428
Gavinsukh <84189346+Gavinsukh@users.noreply.github.com>
2022-01-06 20:09:37 +0700

    Update agenda.blade.php

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.sidebar')
 @section('content')
 
+<div class="rightContainer" style=
+"width:60vw;
+height:40vw;
+border: 1vw solid #FFFFFF; 
+border-radius:0.7vw;
+display:flex;
+flex-direction:column;
+color:#708090;
+justify-content:space-evenly;
+box-shadow: 0.5vw 0.5vw 1vw 0.8vw #C0C0C0;">
 
+</div>
 
 @endsection
