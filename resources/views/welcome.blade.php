c27c263699c3719e78b81aaea01c6ddf9da37279
vanessagiovani <vanessa.giovani@binus.ac.id>
2022-01-06 15:51:36 +0700

    explore oi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
             Your personal task manager, <br> get started now.
         </p>
 
-    <a class="btn" style="background-color: white;color:rgba(200,160,160,255);" href="/register" role="button">Get Started</a>
+        <a class="btn" style="background-color: white;color:rgba(200,160,160,255);" href="/register" role="button">Get Started</a>
     </div>
 
     <div class="imageGetStarted" style="margin-left:5vw;margin-bottom:7vw">
@@ -31,7 +31,7 @@
 
 </div>
 
-<div class="TextBox"  id="about" style="display: flex;flex-direction:column;align-items:center;color:#585858;font-family: Montserrat;
+<div class="TextBox" id="about" style="display: flex;flex-direction:column;align-items:center;color:#585858;font-family: Montserrat;
 font-style: normal">
 
     <div class="upperText" style="
@@ -68,7 +68,7 @@ font-style: normal">
         <div class="text" style="
                         font-size:2vw;
                         margin-top:1vw">
-        File Manager
+            File Manager
         </div>
 
         <div class="text" style="font-size:1.3vw">
@@ -90,7 +90,7 @@ font-style: normal">
         <div class="text" style="
                         font-size:2vw;
                         margin-top:1vw">
-        Progress Tracker
+            Progress Tracker
         </div>
 
         <div class="text" style="font-size:1.3vw">
@@ -98,7 +98,7 @@ font-style: normal">
         </div>
 
         <div class="text" style="font-size:1.3vw">
-           or internship.
+            or internship.
         </div>
     </div>
 
@@ -107,21 +107,21 @@ font-style: normal">
                     flex-direction:column;
                     align-items:center;">
 
-    <img src="../storage/dashboard.png" alt="" style="width: 22vw;height:20vw">
+        <img src="../storage/dashboard.png" alt="" style="width: 22vw;height:20vw">
 
-    <div class="text" style="
+        <div class="text" style="
                     font-size:2vw;
                     margin-top:1vw">
-        Dashboard
-    </div>
+            Dashboard
+        </div>
 
-    <div class="text" style="font-size:1.3vw">
-        Keep your things organized with a
-    </div>
+        <div class="text" style="font-size:1.3vw">
+            Keep your things organized with a
+        </div>
 
-    <div class="text" style="font-size:1.3vw">
-        nice and sleek dashboard!
-    </div>
+        <div class="text" style="font-size:1.3vw">
+            nice and sleek dashboard!
+        </div>
 
     </div>
 
@@ -148,6 +148,29 @@ font-style: normal">
 </div>
 
 
+<br>
+<div class="Explore">
+<div class="TextBox" id="about" style="display: flex;flex-direction:column;align-items:center;color:#585858;font-family: Montserrat;
+font-style: normal">
+
+    <div class="upperText" style="
+    font-weight: bold;
+    font-size: 5vw;">
+        Explore
+    </div>
+
+    <div class="lowerText" style="font-size: 2vw">
+        Introduction to UniDashboard
+    </div>
+    <br>
+</div>
+    <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" width="300" height="150" src="https://www.youtube.com/embed/TLahQwBlK6g" frameborder="1" allowfullscreen></iframe>
+    </div>
+</div>
+
+
+
 {{-- style="background-image:url(../storage/footer.png); background-size: 100%" --}}
 <div class="footer" style="
                     font-family: Montserrat;
@@ -158,40 +181,40 @@ font-style: normal">
                     display:flex;
                     justify-content:space-between;
                     background-image:url(../storage/footer.png);background-size: 100%;background-repeat:no-repeat;">
-                    
 
-<div class="TextBox" style="
+
+    <div class="TextBox" style="
                     font-size:1.5vw;
                     color:white;
                     margin-top:8vw;
                     margin-left:5vw;
                     margin-right:2vw">
-    Contact Us <br>
+        Contact Us <br>
 
-    <div class="nomor" style="font-size:1vw">
-        Call +62 0894-3509-4372
+        <div class="nomor" style="font-size:1vw">
+            Call +62 0894-3509-4372
+        </div>
     </div>
-</div>
 
-<a href="#" class="second" style="color: white;margin-top:9.5vw;font-size:1.5vw">Terms and Conditions</a>
-<script>
-    document.querySelector(".second").addEventListener('click', function(){
-  Swal.fire("Terms & Conditions", "We are not responsible for any damages done to your personal, and mental health.");
-});
-</script>
+    <a href="#" class="second" style="color: white;margin-top:9.5vw;font-size:1.5vw">Terms and Conditions</a>
+    <script>
+        document.querySelector(".second").addEventListener('click', function() {
+            Swal.fire("Terms & Conditions", "We are not responsible for any damages done to your personal, and mental health.");
+        });
+    </script>
 
-<a href="#" class="second2" style="color: white;margin-top:9.5vw;font-size:1.5vw">Privacy</a>
-<script>
-    document.querySelector(".second2").addEventListener('click', function(){
-  Swal.fire("Privacy", "We would like to ensure you that your data will be not be hacked.");
-});
-</script>
+    <a href="#" class="second2" style="color: white;margin-top:9.5vw;font-size:1.5vw">Privacy</a>
+    <script>
+        document.querySelector(".second2").addEventListener('click', function() {
+            Swal.fire("Privacy", "We would like to ensure you that your data will be not be hacked.");
+        });
+    </script>
 
-<div class="socmedBox" style="margin-top:8vw; margin-right:5vw">
-    <a href="#" class="fa fa-facebook"></a>
-    <a href="#" class="fa fa-twitter"></a>
-    <a href="#" class="fa fa-instagram"></a>
-</div>
+    <div class="socmedBox" style="margin-top:8vw; margin-right:5vw">
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-twitter"></a>
+        <a href="#" class="fa fa-instagram"></a>
+    </div>
 
 
 </div>
