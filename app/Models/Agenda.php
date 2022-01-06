9062787b0c488af5fadac1b8a49531a2d6912aa7
Gavinsukh <84189346+Gavinsukh@users.noreply.github.com>
2022-01-06 19:57:52 +0700

    dsadsadsa

--- app/Models/Agenda.php
+++ app/Models/Agenda.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Agenda extends Model
 {
     use HasFactory;
-
+    protected $fillable = ['user_id'];
     public function User(){
         return $this->belongsTo(User::class);
     }
